366f8fced75e5178423375fa6e4ee782f8ac8cfb
stebucko360 <stebucko360@gmail.com>
2021-11-29 12:29:07 +0000

    created all tables succesfully

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -7,14 +7,14 @@ const seed = (data) => {
   
   // DROP TABLES IF EXIST
 
-  return db.query(`DROP TABLE IF EXISTS catergories;`)
+  return db.query(`DROP TABLE IF EXISTS comments;`)
   .then(()=>{
-    return db.query(`DROP TABLE IF EXISTS users`)
+    return db.query(`DROP TABLE IF EXISTS reviews;`)
   })
   .then(()=>{
-    return db.query(`DROP TABLE IF EXISTS reviews`)
+    return db.query(`DROP TABLE IF EXISTS users;`)
   }).then(()=>{
-    return db.query(`DROP TABLE IF EXISTS comments`)
+    return db.query(`DROP TABLE IF EXISTS catergories;`)
   })
   .then(()=>{
   //CREATE TABLES
@@ -33,6 +33,32 @@ const seed = (data) => {
       avatar_url VARCHAR(250),
       name VARCHAR(100) NOT NULL
     );`)
+  })
+  .then(()=>{
+
+    return db.query(`
+    CREATE TABLE reviews(
+      review_id SERIAL PRIMARY KEY,
+      title VARCHAR(100) NOT NULL,
+      review_body TEXT NOT NULL,
+      designer VARCHAR(100) NOT NULL,
+      review_img_url VARCHAR(400) DEFAULT E'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      votes INT DEFAULT 0,
+      catergory VARCHAR(100) REFERENCES catergories (slug),
+      owner VARCHAR(100) REFERENCES users (username),
+      created_at DATE DEFAULT CURRENT_TIMESTAMP
+    );`)
+  })
+  .then(()=>{
+    return db.query(`
+  CREATE TABLE comments(
+    comment_id SERIAL PRIMARY KEY,
+    author VARCHAR(100) REFERENCES users (username),
+    review_id INT REFERENCES reviews (review_id),
+    votes INT DEFAULT 0,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP,
+    body TEXT NOT NULL
+  );`)
   })
   
 };
