de29940ebeb3d3046336be3632453d2a9b5dd051
stebucko360 <stebucko360@gmail.com>
2021-11-29 11:51:46 +0000

    completed dropt ables and added tables; users, catergories and reviews

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,40 @@
+const db = require('../connection');
+const pg = require('pg');
+const format = require('pg-format');
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+  
+  // DROP TABLES IF EXIST
+
+  return db.query(`DROP TABLE IF EXISTS catergories;`)
+  .then(()=>{
+    return db.query(`DROP TABLE IF EXISTS users`)
+  })
+  .then(()=>{
+    return db.query(`DROP TABLE IF EXISTS reviews`)
+  }).then(()=>{
+    return db.query(`DROP TABLE IF EXISTS comments`)
+  })
+  .then(()=>{
+  //CREATE TABLES
+
+  return db.query(`
+  CREATE TABLE catergories(
+    slug VARCHAR(100) PRIMARY KEY NOT NULL,
+    description VARCHAR(200)
+  );`)
+  })
+  .then(()=>{
+
+    return db.query(`
+    CREATE TABLE users(
+      username VARCHAR(100) PRIMARY KEY NOT NULL,
+      avatar_url VARCHAR(250),
+      name VARCHAR(100) NOT NULL
+    );`)
+  })
+  
 };
 
 module.exports = seed;
