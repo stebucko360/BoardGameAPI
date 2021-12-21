0f15fbdd3e93f54baf4baa1bf893eaf44b816674
stebucko360 <stebucko360@gmail.com>
2021-12-21 12:44:49 +0000

    altered GET /api test for new endpoint checks

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -429,7 +429,7 @@ describe('GET /api', ()=>{
         const endPoints = {
             'GET: /api/categories': 'Responds with an array of category objects',
             'GET: /api/reviews/:review_id': 'Responds with a review object with the defined review_id',
-            'GET: /api/reviews': 'Responds with a reviews array containg all review objects; can be used with queries: sort_by, order and category',
+            'GET: /api/reviews': 'Responds with a reviews array containg all review objects; can be used with queries: sort_by, order and category. This endpoint also has pagination as a feature just use the queries limit and page',
             'GET: /api/reviews/:review_id/comments': 'Responds with an array of comments for the given review_id',
             'GET: /api': 'Where you are right now!',
             'GET: /api/users': 'Responds with an array of objects containing usernames',
@@ -437,6 +437,7 @@ describe('GET /api', ()=>{
             'PATCH: /api/reviews/:review_id': 'Must send an object in the form of {inc_votes: number}, will respond with the patched review',
             'PATCH: /api/comments/:comment_id': 'Must send an object in the form of { inc_votes: 1 | -1 } depending on increment or decrement. Responds with the requested comment with votes added/subtracted',
             'POST: /api/reviews/:review_id/comments': 'Must send an object in the form of {username: string, body: string}, will respond with the new posted comment',
+            'POST: /api/reviews': 'Must send an object with the following parameters: owner (username), title, review_body, designer and a valid category',
             'DELETE: /api/comments/:comment_id': 'Delete the requested comment_id, will respond with "204"'
         };
 
