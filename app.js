33328424a239d1b3efecca73bd8a3235ad195c31
stebucko360 <stebucko360@gmail.com>
2022-01-10 13:28:46 +0000

    added cors to backend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { handlePSQLErrors, handleCustomErrors, handleServerErrors } = require('./error_handling');
 const apiRouter = require('./routers/api.router');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
