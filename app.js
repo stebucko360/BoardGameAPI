b47ac3b39173187259517bea5c57e15200a7be87
Stephen Buckley <stephenbuckley@Stephens-MacBook-Pro.local>
2022-04-21 19:22:01 +0100

    test upload

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const { handlePSQLErrors, handleCustomErrors, handleServerErrors } = require('./error_handling');
-const apiRouter = require('./routers/api.router');
-const cors = require('cors');
+const express = require("express");
+const {
+  handlePSQLErrors,
+  handleCustomErrors,
+  handleServerErrors,
+} = require("./error_handling");
+const apiRouter = require("./routers/api.router");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', apiRouter);
-app.get('/', (req,res)=>{
-    res.status(200).send({msg: "🧔Welcome to Stephen's Board game API, use the endpoint /api to see all available endpoints💾"}
-    )});
+app.use("/api", apiRouter);
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({
+      msg: "🧔Welcome to Stephen's Board game API, use the endpoint /api to see all available endpoints💾",
+    });
+});
 
 app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
 
+//test
 
 module.exports = app;
